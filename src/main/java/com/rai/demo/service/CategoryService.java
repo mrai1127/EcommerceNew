a70a4235fc0f03e6a19e10c6f30e8d32c6bec12e
Mausam Rai <mausamrai11@gmail.com>
2024-04-24 14:37:27 -0500

    rest changes for category

--- src/main/java/com/rai/demo/service/CategoryService.java
+++ src/main/java/com/rai/demo/service/CategoryService.java
@@ -2,15 +2,21 @@ package com.rai.demo.service;
 
 import com.rai.demo.model.Category;
 import com.rai.demo.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
-
+    @Autowired
     private CategoryRepository categoryRepository;
 
     public void createCategory(Category category){
         categoryRepository.save(category);
+    }
 
+    public List<Category> listCategory(){
+       return categoryRepository.findAll();
     }
 }
