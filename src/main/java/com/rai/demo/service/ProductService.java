de5aca25ffccd5736eaec6d024d154fe72b4fb42
Mausam Rai <mausamrai11@gmail.com>
2024-05-01 23:48:55 -0500

    previous changes back in

--- src/main/java/com/rai/demo/service/ProductService.java
+++ src/main/java/com/rai/demo/service/ProductService.java
@@ -5,7 +5,6 @@ import com.rai.demo.model.Category;
 import com.rai.demo.model.Product;
 import com.rai.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
